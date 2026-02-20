ea7917c150bc317c9d4a1434145054f9ac9ee277
Raphael Miller <gemini88mill@gmail.com>
2026-02-20 14:07:54 -0500

    DateLabel

--- src/components/atom/DateLabel/DateLabel.tsx
+++ src/components/atom/DateLabel/DateLabel.tsx
@@ -10,7 +10,7 @@ const formatDateLabel = (dateKey: string) => {
 
 export const DateLabel = ({ dateKey }: DateLabelProps) => {
   return (
-    <div className="flex h-12 w-12 items-center justify-center rounded-sm border border-slate-400 bg-slate-100 text-xs font-semibold text-slate-700">
+    <div className="flex h-12 w-12 items-center justify-center rounded-sm bg-slate-100 text-xs font-semibold text-slate-700">
       {formatDateLabel(dateKey)}
     </div>
   )
